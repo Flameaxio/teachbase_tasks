f7596b77bb4727a888b95410753c7971eff911d4
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-02-16 15:54:52 +0200

    Added input validation

--- triangle_area.rb
+++ triangle_area.rb
@@ -2,4 +2,8 @@ print 'Enter the base of the triangle(a): '
 base = gets.chomp.to_f
 print 'Enter the height of the triangle(h): '
 height = gets.chomp.to_f
-print "The area of this triangle is #{base * height * 0.5}"
+if base.positive? && height.positive?
+  print "The area of this triangle is #{base * height * 0.5}"
+else
+  print 'Wrong input!'
+end
