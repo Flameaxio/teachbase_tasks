ec1f8c9edb7d6807277d82a6940a8857c4f2c0af
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-02-17 15:04:59 +0200

    Added variable that stores all instances of the class

--- station.rb
+++ station.rb
@@ -1,9 +1,16 @@
 class Station
   attr_reader :name
 
+  @@all_stations = []
+
   def initialize(name)
     @name = name
     @trains = []
+    @@all_stations.push(self)
+  end
+
+  def self.all
+    @@all_stations
   end
 
   def accept_train(train)
