0629fefc62d7a21fb0055b26952c8af9fc56dc41
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-02-17 17:45:46 +0200

    Expanded classes

--- cargo_carriage.rb
+++ cargo_carriage.rb
@@ -2,6 +2,7 @@ require_relative 'carriage'
 
 class CargoCarriage < Carriage
   attr_reader :capacity
+  attr_accessor :load
 
   def initialize(type, capacity)
     super(type)
@@ -13,6 +14,15 @@ class CargoCarriage < Carriage
     raise RangeError, 'Capacity should be > 0' if @capacity <= 0
 
     true
+  end
+
+  def load_cargo(amount)
+    'Not enough capacity' if @load + amount > @capacity || amount.negative?
+    @load += amount
+  end
+
+  def free_space
+    @capacity - @load
   end
 
 end
--- passenger_carriage.rb
+++ passenger_carriage.rb
@@ -2,6 +2,7 @@ require_relative 'carriage'
 
 class PassengerCarriage < Carriage
   attr_reader :number_of_seats
+  attr_accessor :occupied_seats
 
   def initialize(type, number_of_seats)
     super(type)
@@ -12,6 +13,15 @@ class PassengerCarriage < Carriage
     raise RangeError, 'Number of seats should be > 0' if @number_of_seats <= 0
 
     true
+  end
+
+  def occupy_seat
+    'Full' if @occupied_seats == @number_of_seats
+    @occupied_seats += 1
+  end
+
+  def free_seats
+    @number_of_seats - @occupied_seats
   end
 
 end
