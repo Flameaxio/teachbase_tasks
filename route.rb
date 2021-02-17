db13a5d6bbaf3b2fa3c21ec83b962b5c159a249e
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-02-17 10:40:31 +0200

    Added get_stations method

--- route.rb
+++ route.rb
@@ -19,4 +19,18 @@ class Route
     puts @in_between_stations
     puts @ending_station
   end
+
+  def get_stations(index)
+    if index.zero?
+      puts "Current station (at start): #{@starting_station.name}\n" \
+      "Next station: #{@in_between_stations[index].name}"
+    elsif index == @in_between_stations.size
+      puts "Current station (last station): #{@ending_station.name}\n" \
+      "Previous station: #{@in_between_stations.last}"
+    else
+      puts "Current station: #{@in_between_stations[index + 1]}\n" \
+      "Next station: #{@in_between_stations[index + 2].nil? ? @ending_station : @in_between_stations[index + 2]}\n" \
+      "Previous station: #{@in_between_stations[index]}"
+    end
+  end
 end
