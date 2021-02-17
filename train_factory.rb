8be57ac8e4283dc1b52e2dee92389a315a7ea8bd
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-02-17 17:12:21 +0200

    Created train factory

---
+++ train_factory.rb
@@ -0,0 +1,13 @@
+class TrainFactory
+
+  def self.get_train(number, type)
+    return PassengerTrain.new(number) if type == :passenger.to_s
+    return CargoTrain.new(number) if type == :cargo.to_s
+
+    raise ArgumentError, 'Wrong type'
+  end
+
+  private
+
+  def initialize; end
+end
