1d538a7346fa39fe21a22305974cc9ad47eba06f
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-02-18 13:47:13 +0200

    Expanded user interface

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ require_relative 'passenger_carriage'
 require_relative 'cargo_train'
 require_relative 'cargo_carriage'
 
-route = Route.new(Station.new('Перемога'), Station.new('Метробудiвникiв'))
+route = Route.new(Station.new('Перемога'), Station.new('Метробудівників'))
 route.add_station(Station.new('Олексіївська'))
 route.add_station(Station.new('23-го серпня'))
 route.add_station(Station.new('Ботанічний сад'))
@@ -16,14 +16,20 @@ route.add_station(Station.new('Наукова'))
 route.add_station(Station.new('Держпром'))
 route.add_station(Station.new('Архітектора Бекетова'))
 route.add_station(Station.new('Захисників України'))
-begin
-  puts 'Enter the number of the train: '
-  number = gets.chomp
-  puts 'Enter the type of the train (passenger or cargo): '
-  type = gets.chomp
-  train = TrainFactory.get_train(number, type)
-  puts "Created train: #{train}"
-rescue ArgumentError => e
-  puts e.message
-  retry
+loop do
+  begin
+    puts 'Enter the number of the train (or stop): '
+    number = gets.chomp
+    break if number == 'stop'
+
+    puts 'Enter the type of the train (passenger or cargo): '
+    type = gets.chomp
+    train = TrainFactory.get_train(number, type)
+    puts "Created train: #{train}"
+    route.starting_station.accept_train(train)
+  rescue ArgumentError => e
+    puts e.message
+    retry
+  end
 end
+puts "All trains on station 1: #{route.starting_station.show_trains}"
