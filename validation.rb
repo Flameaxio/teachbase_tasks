38f31d6607ce2539077841d7abb06cc62ff12bd5
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-02-19 12:45:23 +0200

    Created validation module

---
+++ validation.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+# Adds validation for specified attribute
+module Validation
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  # Contains logic for creating validation
+  module ClassMethods
+    def validate(attribute_name, validation_type, option: true)
+      if validation_hash[attribute_name]
+        add_validation(attribute_name, validation_type, option)
+      else
+        create_validation(attribute_name, validation_type, option)
+      end
+    end
+
+    def validation_hash
+      @validation_hash ||= {}
+    end
+
+    private
+
+    def validation_errors(type, attribute_name)
+      case type
+      when :presence
+        "#{attribute_name} shouldn't be nil or ''"
+      when :format
+        "#{attribute_name} has invalid format"
+      when :type
+        "#{attribute_name} has invalid type"
+      else
+        'Unknown error'
+      end
+    end
+
+    def validation_types
+      %i[presence format type]
+    end
+
+    def add_validation(attribute_name, validation_type, option)
+      validation_hash[attribute_name][validation_type] = {
+        option: option,
+        error_message: validation_errors(validation_type, attribute_name)
+      }
+    end
+
+    def create_validation(attribute_name, validation_type, option)
+      validation_hash[attribute_name] = {
+        validation_type => {
+          option: option,
+          error_message: validation_errors(validation_type, attribute_name)
+        }
+      }
+    end
+  end
+
+  # Contains main validation logic
+  module InstanceMethods
+    def validate!
+      self.class.validation_hash.each do |attribute_name, type|
+        type.each do |key, value|
+          send("#{key}_validation", value[:error_message], attribute_name, value[:option])
+        end
+      end
+      true
+    end
+
+    def valid?
+      validate!
+    rescue ArgumentError
+      false
+    end
+
+    def presence_validation(error_message, attribute_name, option: true)
+      raise ArgumentError, error_message if [nil, ''].include?(public_send(attribute_name))
+
+      option
+    end
+
+    def format_validation(error_message, attribute_name, option: true)
+      raise ArgumentError, error_message unless public_send(attribute_name) =~ option
+    end
+
+    def type_validation(error_message, attribute_name, option: true)
+      raise ArgumentError, error_message unless public_send(attribute_name).is_a?(option)
+    end
+  end
+end
