0fe310c15b14f6be408132b77b1a759a1f336977
Olexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2021-02-22 13:49:18 +0200

    Added missing controls

--- main.rb
+++ main.rb
@@ -24,7 +24,8 @@ class Main
     3 => :add_carriage,
     4 => :remove_carriage,
     5 => :add_train_to_station,
-    6 => :trains_on_station
+    6 => :trains_on_station,
+    7 => :control_train
   }.freeze
   attr_reader :stations, :trains
 
@@ -70,6 +71,7 @@ class Main
 4 - Remove carriages from the train
 5 - Put trains on the station
 6 - List all the stations and trains on them
+7 - Control the train
 Enter number:"
     gets.chomp.to_i
   end
